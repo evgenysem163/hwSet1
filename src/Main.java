b21f97ac8fc6a3339d85418e0ef08e16b17c3c9c
evgeny_sem163 <evg82tol@gmail.com>
2023-04-05 22:27:25 +0300

    set

---
+++ src/Main.java
@@ -0,0 +1,77 @@
+import java.util.*;
+
+
+public class Main {
+    private static final Random RANDOM = new Random();
+
+
+    public static void main(String[] args) {
+        List<Integer> nums = generate(10);
+        System.out.println("Сгенерированные числа " + nums);
+        task1(nums);
+        task2(nums);
+        task3(List.of("test", "TEST", "test", "TeSt", "test", "teST"));
+        task4(List.of("test", "TEST", "test", "TeSt", "test", "teST"));
+    }
+
+    private static void task1(List<Integer> nums) {
+        System.out.println("Задача 1");
+        for (Integer number : nums) {
+            if (number % 2 == 1) {
+                System.out.print(number + " ");
+            }
+        }
+        System.out.println();
+
+    }
+
+    private static void task2(List<Integer> nums) {
+        System.out.println("Задача 2");
+        Set<Integer> set = new TreeSet<>(nums);
+        for (Integer number : set) {
+            if (number % 2 == 0) {
+                System.out.print(number + " ");
+            }
+        }
+        System.out.println();
+
+
+    }
+
+    private static void task3(List<String> words) {
+        System.out.println("Задача 3");
+        for (String word : words) {
+            if (Collections.frequency(words, word) == 1) {
+                System.out.print(word + " ");
+            }
+        }
+        System.out.println();
+
+    }
+
+    public static void task4(List<String> words) {
+        System.out.println("Задача 4");
+        Map<String, Integer> map = new TreeMap<>();
+        for (String word : words) {
+            if (!map.containsKey(word)) {
+                map.put(word, 1);
+            } else {
+                map.replace(word, map.get(word) + 1);
+            }
+        }
+        System.out.println(map);
+    }
+
+    private static List<Integer> generate(int n) {
+        List<Integer> list = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            list.add(RANDOM.nextInt(5));
+        }
+        return list;
+
+    }
+
+
+}
+
+
